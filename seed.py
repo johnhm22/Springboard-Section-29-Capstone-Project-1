a580416a3e2df6376627cf12494ec7bdb2e1affb
John Morgan <johnhmorgan@outlook.com>
2021-02-05 14:36:12 +0000

    adding functions to import

--- seed.py
+++ seed.py
@@ -2,8 +2,7 @@
 
 from models import db
 from app import app
-import populate_scripts
-
+from populate_scripts import populate_team_table, populate_season_league_table, populate_team_info_table, populate_standings_table, populate_results_all_table, populate_fixtures
 
 #Create all tables
 db.drop_all()
