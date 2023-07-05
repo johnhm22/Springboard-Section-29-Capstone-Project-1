83a91eb7c86a06fc49c42eb0991e6170bfbd178c
john <johnhowardmorgan@outlook.com>
2023-07-05 14:16:09 +0100

    adding minor adjustment to env variables

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ API_BASE_URL = "https://api-football-v1.p.rapidapi.com/v2"
 # app.config['SQLALCHEMY_DATABASE_URI'] = (
 #     os.environ.get('DATABASE_URL', 'postgres:///matchday'))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-app.config['API_KEY'] = os.getenv('API_KEY')
+app.config['API_KEY'] = os.environ.get('API_KEY')
 API_KEY = app.config['API_KEY']
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
