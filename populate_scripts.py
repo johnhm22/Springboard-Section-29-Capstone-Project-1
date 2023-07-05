fafe763a9a3b28922da8566df497421939da0250
john <johnhowardmorgan@outlook.com>
2023-07-05 14:17:16 +0100

    defining environment key API_KEY

--- populate_scripts.py
+++ populate_scripts.py
@@ -18,7 +18,7 @@ API_BASE_URL = "https://api-football-v1.p.rapidapi.com/v2"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-# app.config['API_KEY'] = (os.environ.get('API_KEY', API_KEY_SECRETS_FILE))
+app.config['API_KEY'] = os.environ.get('API_KEY')
 API_KEY = app.config['API_KEY']
 
 
